657dc16ba0672ffa2d532c155a58bf240f2858ae
nicolec2546 <144353597+nicolec2546@users.noreply.github.com>
2024-03-19 14:12:39 +0000

    m

---
+++ script.js
@@ -0,0 +1,11 @@
+let score = document.getElementById('score');;
+let totalScore = 0
+
+
+const btn = document.querySelector(".cookie");
+
+if(btn){
+    btn.addEventListener("click",nextScreen);
+    score++;
+}
+
