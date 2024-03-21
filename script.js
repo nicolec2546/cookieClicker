5477055fc30504c6733182b13a76c0a4ca37a9ba
nicolec2546 <144353597+nicolec2546@users.noreply.github.com>
2024-03-21 14:14:23 +0000

    m

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-let score = document.getElementById('score');;
+let score = document.getElementById('cookie');;
 let totalScore = 0
 
 
 const btn = document.querySelector(".cookie");
 
 if(btn){
-    btn.addEventListener("click",nextScreen);
+    btn.addEventListener("click","cookie");
     score++;
+    cookie.style.width = 400;
 }
 
